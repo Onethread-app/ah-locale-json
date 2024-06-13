b986da898ef2546be7f9211dab508ff9fe9f4518
Joy Bordhen <jbordhen.jb@gmail.com>
2024-06-13 10:44:23 +0600

    Added locale files

---
+++ locales/index.ts
@@ -0,0 +1,15 @@
+import { bn } from './bengali'
+import { cn } from './chinese'
+import { cz } from './czech'
+import { de } from './dutch'
+import { en } from './english'
+import { fr } from './french'
+import { jp } from './japanese'
+import { kr } from './korean'
+import { es } from './spanish'
+
+const locales = { en, bn, cn, cz, de, fr, jp, kr, es }
+
+export { en, bn, cn, cz, de, fr, jp, kr, es }
+
+export default locales
---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import resolve from '@rollup/plugin-node-resolve'
+import typescript from '@rollup/plugin-typescript'
+import terser from '@rollup/plugin-terser'
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+const config = [
+  {
+    input: 'locales/index.ts',
+    output: [
+      {
+        dir: './lib',
+        format: 'esm',
+        sourcemap: true,
+        preserveModules: true,
+        preserveModulesRoot: 'locales',
+      },
+    ],
+    plugins: [resolve(), typescript({ tsconfig: './tsconfig.json' }), terser()],
+  },
+]
+
+export default config
