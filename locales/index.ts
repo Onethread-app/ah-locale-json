3d4c81e84f795d8664c9ba19ce4e8310fc8a9714
Joy Bordhen <jbordhen.jb@gmail.com>
2024-06-24 15:30:38 +0600

    Updated folder structure

--- locales/index.ts
+++ locales/index.ts
@@ -1,15 +1,4 @@
-import { bn } from './bengali'
-import { cn } from './chinese'
-import { cz } from './czech'
-import { de } from './dutch'
-import { en } from './english'
-import { fr } from './french'
-import { jp } from './japanese'
-import { kr } from './korean'
-import { es } from './spanish'
+import fe from './frontend'
+import be from './backend'
 
-const locales = { en, bn, cn, cz, de, fr, jp, kr, es }
-
-export { en, bn, cn, cz, de, fr, jp, kr, es }
-
-export default locales
+export default { fe, be }
