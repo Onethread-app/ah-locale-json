151624e444b244280c313a0541c7bf331601c179
Joy Bordhen <jbordhen.jb@gmail.com>
2024-08-02 16:08:47 +0600

    Added Norwegian and Latvian language

--- locales/frontend/index.ts
+++ locales/frontend/index.ts
@@ -8,8 +8,10 @@ import { jp } from './japanese'
 import { kr } from './korean'
 import { es } from './spanish'
 import { italian } from './italian'
+import { latvian } from './latvian'
+import { no } from './norwegian'
 
-const locales = { en, bn, cn, cz, de, fr, jp, kr, es, italian }
+const locales = { en, bn, cn, cz, de, fr, jp, kr, es, italian, latvian, no }
 
 export { en, bn, cn, cz, de, fr, jp, kr, es, italian }
 
