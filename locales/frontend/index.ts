449cd92cd2175695ba95fc0205c8f09b330dc896
Joy Bordhen <24916865+Jbordhen@users.noreply.github.com>
2024-08-07 11:41:04 +0600

    Feat/italian frontend (#7)
    * Added italian language text
    
    * Added Norwegian and Latvian language

--- locales/frontend/index.ts
+++ locales/frontend/index.ts
@@ -7,9 +7,12 @@ import { fr } from './french'
 import { jp } from './japanese'
 import { kr } from './korean'
 import { es } from './spanish'
+import { italian } from './italian'
+import { latvian } from './latvian'
+import { no } from './norwegian'
 
-const locales = { en, bn, cn, cz, de, fr, jp, kr, es }
+const locales = { en, bn, cn, cz, de, fr, jp, kr, es, italian, latvian, no }
 
-export { en, bn, cn, cz, de, fr, jp, kr, es }
+export { en, bn, cn, cz, de, fr, jp, kr, es, italian }
 
 export default locales
